efcde86f9408db5605b8283ef48caa28956ad7bf
Diablohu <Diablohu@users.noreply.github.com>
2019-04-23 14:18:34 +0800

    update samples

--- react-ssr-server/react/app.jsx
+++ react-ssr-server/react/app.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const App = ({ request = {} }) => (
-    <div id="app">
+    <div
+        id="app"
+        style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            display: 'flex',
+            width: '100vw',
+            height: '100vh',
+            flexFlow: 'column nowrap',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}
+    >
         <h1>Hello World!</h1>
-        <p>Current request href: {request.href}</p>
+        <p>Current request href: <em>{request.href}</em></p>
     </div>
 )
 
